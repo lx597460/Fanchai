74fb1c2db185fe5be6e9f105e7c8fab37e9a4f91
lx597460 <lx597460@gmail.com>
2026-09-19 04:20:45 +0800

    更新 StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -68,7 +68,7 @@ class StoryMenuState extends MusicBeatState
 		#if MOD_STATES_ALLOWED
 		checkModScript();
 		if (modScript != null) {
-			modScript.setVariable('state', this);
+			modScript.set('state', this);
 			if (modScript.exists('onCreate')) {
 				modScript.call('onCreate');
 				super.create();
@@ -216,11 +216,11 @@ class StoryMenuState extends MusicBeatState
 		modScriptChecked = true;
 
 		#if HSCRIPT_ALLOWED
-		var path = Paths.getPreloadPath('states/StoryMenuState.hx');
+		var path = Paths.getSharedPath('mods/states/StoryMenuState.hx');
 		if (path != null && FileSystem.exists(path)) {
 			try {
-				modScript = new HScript();
-				modScript.execute(File.getContent(path));
+				modScript = new HScript(null, path);
+				modScript.doString(File.getContent(path));
 				trace('[ModStates] Loaded: ' + path);
 			} catch (e:Dynamic) {
 				trace('[ModStates] Load failed: ' + e);
@@ -247,8 +247,8 @@ class StoryMenuState extends MusicBeatState
 		// === 路线A新增：mod 脚本接管 update ===
 		#if MOD_STATES_ALLOWED
 		if (modScript != null && modScript.exists('onUpdate')) {
-			modScript.setVariable('state', this);
-			modScript.setVariable('elapsed', elapsed);
+			modScript.set('state', this);
+			modScript.set('elapsed', elapsed);
 			modScript.call('onUpdate');
 		}
 		#end
